af36c014df01f6e0065e434ccd62ea2bc0e20e05
Ozkan Ozesme <ozkanozsm@gmail.com>
2021-12-12 17:53:22 +0300

    Fix cors issue

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins("*");
+    }
+    );
+}
+);
+
 
 var app = builder.Build();
 
@@ -19,7 +29,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseCors();
+app.UseCors(MyAllowSpecificOrigins);
 
 
 app.UseHttpsRedirection();
