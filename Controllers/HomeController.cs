7ff8f193d41bd161c3598a41443a2226181deff2
Ozkan Ozesme <ozkanozsm@gmail.com>
2021-12-26 21:32:27 +0300

    Redirect root page to swagger ui

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YemekTBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [Route(""), HttpGet]
+        public RedirectResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger/");
+        }        
+    }
+}
